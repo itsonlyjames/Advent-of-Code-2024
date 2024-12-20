c4e06cf2b23d89d49193c2529e01d003aac94e80
itsonlyjames <jamesalexandre1995@gmail.com>
2024-12-21 00:32:01 +1100

    Day 19, 20, and 21

--- 19/solve.py
+++ 19/solve.py
@@ -1,1 +1,32 @@
+import re
+from functools import lru_cache
+
 from aocd import data
+
+patterns, designs = data.split("\n\n")
+patterns = patterns.split(", ")
+pattern = "|".join(patterns)
+
+
+@lru_cache(None)
+def count(design):
+    if not design:
+        return 1
+    total = 0
+    for pattern in patterns:
+        if design.startswith(pattern):
+            total += count(design[len(pattern) :])
+    return total
+
+
+possible = 0
+total = 0
+
+for design in designs.splitlines():
+    match = re.fullmatch(f"({pattern})+", design)
+    if match:
+        possible += 1
+    total += count(design)
+
+
+print(f"p1: {possible}, p2: {total}")
